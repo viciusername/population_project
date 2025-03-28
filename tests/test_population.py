a065c9e320f648cd4440b8f4ecfdd30bf4757bd4
unknown <gumimigu8@gmail.com>
2025-03-28 23:35:50 +0200

    Налаштування CI для автоматичних тестів

--- tests/test_population.py
+++ tests/test_population.py
@@ -4,14 +4,25 @@ from population_sort import read_population_data, sort_by_area, sort_by_populati
 @pytest.fixture
 def sample_data():
     return [
-        ("Країна A", 1000000, 50000000),
-        ("Країна B", 2000000, 30000000),
-        ("Країна C", 1500000, 70000000)
+        ("Країна А", 1000000, 5000000),
+        ("Країна В", 2000000, 3000000),
+        ("Країна С", 1500000, 7000000),
     ]
 
-@pytest.mark.parametrize("func,expected", [
-    (sort_by_area, [("Країна B", 2000000, 30000000), ("Країна C", 1500000, 70000000), ("Країна A", 1000000, 50000000)]),
-    (sort_by_population, [("Країна C", 1500000, 70000000), ("Країна A", 1000000, 50000000), ("Країна B", 2000000, 30000000)])
-])
+@pytest.mark.parametrize(
+    "func,expected",
+    [
+        (sort_by_area, [
+            ("Країна В", 2000000, 3000000),
+            ("Країна С", 1500000, 7000000),
+            ("Країна А", 1000000, 5000000),
+        ]),
+        (sort_by_population, [
+            ("Країна С", 1500000, 7000000),
+            ("Країна А", 1000000, 5000000),
+            ("Країна В", 2000000, 3000000),
+        ]),
+    ]
+)
 def test_sorting_functions(sample_data, func, expected):
     assert func(sample_data) == expected
